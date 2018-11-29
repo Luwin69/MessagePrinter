3fc4b8576915bd3d402c3d318985521ef8a1b0a8
Luwin69 <42574052+Luwin69@users.noreply.github.com>
2018-11-29 15:42:36 +0800

    Add files via upload
    start and add base method

---
+++ OutputMessage/BaseMethod.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OutputMessage
+{
+    public static class BaseMethod
+    {
+        internal List<string> keyWords = new List<string>();
+        internal List<string> filePath = new List<string>();
+        public static void WriteLine(string path, string lineText)
+        {
+            if (path == null) return;
+            FileStream wl = new FileStream(path, FileMode.OpenOrCreate);
+            StreamReader textWriter = new StreamReader(wl, Encoding.Default);
+            textWriter.Write(lineText);
+        }
+        public static string GetFilePath(string key)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "*.txt|*.txt";
+            dlg.Title = text + "存储路径";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                return dlg.FileName;
+            }
+            return null;
+        }
+        public static void SetFilePath()
+        {
+            for (int i = 0; i < keyWords.Count; i++)
+            {
+                filePath.Add(GetFilePath(keyWords[i]));
+            }
+        }
+        public static void OpenKeyWordsFile(string path)
+        {
+            StreamReader textReader = new StreamReader(path, Encoding.Default);
+            string keyWord = "";
+            while (true)
+            {
+                string textLine = textReader.ReadLine();
+                if (textLine != null)
+                {
+                    keyWords.Add(textLine);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            textReader.Close();
+        }
+    }
+}
